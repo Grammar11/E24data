d83f0f39704c2c2337b5f4efdc0d7b0cf85b9310
Grammar11 <108354220+Grammar11@users.noreply.github.com>
2026-07-31 10:15:05 +0100

    Fix Airtel data plan values
    Updated Airtel_1GB and Airtel_3GB values.

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ const SMEAPI_PLAN_IDS = {
   MTN_1GB: 2,
   MTN_2GB: 3,
   MTN_3GB: 4,
-  Airtel_1GB: 71,
+  Airtel_1GB: 75,
   Airtel_2GB: 79,
+  Airtel_3GB: 84,
   Glo_1GB: 113,
   'Glo_2.5GB': 121,
   Glo_3GB: 123
