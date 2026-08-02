652e92da64490e4431534949772654917565ab58
Grammar11 <108354220+Grammar11@users.noreply.github.com>
2026-08-02 06:16:00 +0100

    Increase JSON body limit to 10mb

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PDFDocument = require('pdfkit');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
