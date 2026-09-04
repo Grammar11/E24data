a4fa202ccdda419923f4cb75a632da870e08cc06
Grammar11 <108354220+Grammar11@users.noreply.github.com>
2026-09-04 09:42:51 +0100

    Refactor USSD endpoint to use enqueueRedeem

--- server.js
+++ server.js
@@ -228,7 +228,7 @@ function normalizePhone(phone) {
   return phone;
 }
 app.post('/api/ussd', async (req, res) => {
-  
+  await enqueueRedeem(async () => {
   const { phoneNumber, text } = req.body;
   res.set('Content-Type', 'text/plain');
 
@@ -343,8 +343,8 @@ app.post('/api/ussd', async (req, res) => {
     return res.send(
       'END Sorry, something went wrong. Please try again later.'
     );
- }
-    
+   }
+    });
   });
 
 app.post('/api/cards/pdf', (req, res) => {
